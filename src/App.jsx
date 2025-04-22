34275b447c3b4281a6f21c1fbf75ceb48299adaf
Karan Kumar <mrkarankr87@gmail.com>
2025-04-22 17:50:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const images = [
- '/public/pic1.jpg',
+ './public/pic1.jpg',
  pic2.jpg,
  pic3.jpg,
  pic4.jpg,
@@ -21,7 +21,6 @@ export default function App() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
     autoplaySpeed: 2000
   };
 
